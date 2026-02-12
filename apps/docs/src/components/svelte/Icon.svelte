62a5703054a32f9bd28c1ae022e398edabddac9b
jaftdelgado <jaftdelgado@gmail.com>
2026-02-12 00:54:41 -0600

    feat(button): Add Button start & end content props

---
+++ apps/docs/src/components/svelte/Icon.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { icons, type IconName } from "@assets/icons/icons";
+
+  export let name: IconName;
+  export let size: number = 16;
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width={size}
+  height={size}
+  viewBox="0 0 16 16"
+  fill="none"
+  class={$$props.class}
+  {...$$restProps}
+>
+  {@html icons[name]}
+</svg>
---
+++ apps/docs/src/examples/svelte/button/ButtonContent.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Button } from "@shizen-ui/svelte";
+  import Icon from "@components/svelte/Icon.svelte";
+</script>
+
+<div class="flex items-center gap-2">
+  <Button>
+    {#snippet startContent()}
+      <Icon name="square_brackets_bars_vertical" />
+    {/snippet}
+    Scan Article
+  </Button>
+
+  <Button variant="tertiary">
+    {#snippet endContent()}
+      <Icon name="folder_arrow_up_in" />
+    {/snippet}
+    Upload Files
+  </Button>
+</div>
