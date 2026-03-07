37d2bef66126e9f948bc6b7b1032d22fa5f6c734
jaftdelgado <jaftdelgado@gmail.com>
2026-03-07 17:42:42 -0600

    fix(apps/docs): fixed Astro hydratation issue

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -12,9 +12,6 @@ export default defineConfig({
     plugins: [tailwindcss()],
     ssr: {
       noExternal: ["@shizen-ui/styles", "@shizen-ui/svelte"]
-    },
-    optimizeDeps: {
-      include: ["@shizen-ui/svelte"]
     }
   }
 });
